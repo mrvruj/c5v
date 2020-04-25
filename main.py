8386ce6d6d9f7fa41d7513268a64d155587ab1b2
vruj <mrvruj@gmail.com>
2020-04-25 00:01:27 -0400

    centered result label

--- main.py
+++ main.py
@@ -747,7 +747,7 @@ class c4(QDialog):
         #simulation run output above the plots 
         self.results.deleteLater()
         self.results = QLabel("This simulation run created " + str("{:,}".format(round(numPx,0))[:-2]) + " patients, representing " + str(round(fracPx,3)*100) + "% of the catchment area (which represents a population of " + str("{:,}".format(round(totalP*catchArea,0))[:-2]) +").")  
-        self.mainLayout.addWidget(self.results, 0, 1, 1, 3) #(row, column, rowspan, columnspan)
+        self.mainLayout.addWidget(self.results, 0, 1, 1, 3, Qt.AlignCenter) #(row, column, rowspan, columnspan)
                 
         reload(LOS_model)
         
